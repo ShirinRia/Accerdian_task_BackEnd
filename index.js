979e836f9a2de35157ab545bf04a5672f288bf99
SHIRIN <efshitaria123@gmail.com>
2023-12-20 01:36:28 +0600

    Backend Deployed

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const express = require('express')
 var cors = require('cors')
 var app = express()
 app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true
+        origin: ['http://localhost:5173'],
+        credentials: true
 
-  }
+    }
 
 ))
 
 app.use(express.json())
 const port = process.env.PORT || 5000
-const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:'',
-    database:'accredian_task'
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: '',
+    database: 'accredian_task'
 })
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -26,22 +26,22 @@ app.get('/', (req, res) => {
 app.post('/user', async (req, res) => {
     const user = req.body
     console.log(user)
-   
-    var sql = "INSERT INTO users (`name`, `email`, `password`) VALUES (?)"; 
-    const values=[
-       
+
+    var sql = "INSERT INTO users (`_id`,`name`, `email`, `password`) VALUES (?)";
+    const values = [
+        user._id,
         user.name,
         user.email,
         user.password
-    ] ;
-   
-    db.query(sql, [values] ,(err, data)=> {
+    ];
+
+    db.query(sql, [values], (err, data) => {
         if (err) return res.json(err);
-       
+
         return res.json("record inserted");
-      });
-     
-  })
+    });
+
+})
 
 //   get specific user
 app.get('/users/:email/:password', async (req, res) => {
@@ -49,20 +49,32 @@ app.get('/users/:email/:password', async (req, res) => {
     const password = req.params.password;
     console.log(email)
     console.log(password)
-   
-    var sql = "SELECT * FROM users WHERE email = (?)"; 
-    
-   
-    db.query(sql, [email] ,(err, data)=> {
+
+    var sql = "SELECT * FROM users WHERE email = (?)";
+
+
+    db.query(sql, [email], (err, data) => {
         if (err) return res.json(err);
         console.log(data[0].password)
-       if(data[0].password===password){
-        return res.json("found");
-       }
+        if (data[0].password === password) {
+            return res.json("found");
+        }
         return res.json("User Credentials does not match");
-      });
-     
-  })
+    });
+
+})
+//   get all user
+app.get('/users', async (req, res) => {
+
+    var sql = "SELECT * FROM users";
+
+    db.query(sql, (err, data) => {
+        if (err) return res.json(err);
+
+        return res.json(data);
+    });
+    res.send(data)
+})
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
